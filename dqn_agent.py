4baaa9504bf2d5e8c015cc734bdf1be2661ff601
Wajih Ouertani <wajih.ouertani@gmail.com>
2019-10-21 05:55:54 +0200

    update dqn_agent (small refactoring)

--- dqn_agent.py
+++ dqn_agent.py
@@ -2,9 +2,7 @@
 
 import sys
 import random
-import string
-from typing import Tuple, List
-from collections import deque
+from typing import List, Tuple
 
 import numpy as np
 
@@ -13,21 +11,38 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.nn as nn
 
-from q_net import QNetFactory
+from q_net import NNFactory
 from replay_buffer import Experience, BaseReplayBufferFactory
+from experience_replay_agent import ExperienceReplayAgent
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class DQNAgent:
+def to_tensors(experiences: List[Experience], device) -> Tuple[torch.Tensor]:
+    """ Transforms a batch of experiences to tensors for  training/learning
+    TODO: move to_tensors this to a utils/tools deps.
+    """
+    states = torch.from_numpy(
+        np.array([e.state for e in experiences])).float().to(device)
+    actions = torch.from_numpy(
+        np.vstack([e.action for e in experiences])).long().to(device)
+    rewards = torch.from_numpy(
+        np.vstack([e.reward for e in experiences])).float().to(device)
+    next_states = torch.from_numpy(
+        np.array([e.next_state for e in experiences])).float().to(device)
+    dones = torch.from_numpy(
+        np.vstack([e.done for e in experiences]).astype(np.uint8)).float().to(device)
+    return states, actions, rewards, next_states, dones
+
+
+class DQNAgent(ExperienceReplayAgent):
     """Interacts with and learns from the environment."""
 
-    def __init__(self, network_factory: QNetFactory, replay_buffer_factory: BaseReplayBufferFactory,
+    def __init__(self, network_factory: NNFactory, replay_buffer_factory: BaseReplayBufferFactory,
                  seed: int = 0, batch_size: int = 64,
                  step_to_update: int = 5, buffer_size: int = int(1e5), gamma: float = .99,
                  lr: float = 5e-4, tau: float = 1e-3, episodes_window_size: int = 100, name=None):
         """Initialize an Agent object.
-
 
         :param seed: initializes pseudo random gen. random.seed(seed)
         :param batch_size: training batch_size
@@ -38,10 +53,11 @@ class DQNAgent:
         :param tau: Q soft update tau
         :param episodes_window_size: deque storing the [episodes_window_size] last episodes score
         """
-        random.seed(seed)
-
-        self._name = name if name is not None else ''.join(
-            random.choice(string.ascii_letters) for _ in range(8))
+        super(DQNAgent, self).__init__(replay_buffer_factory=replay_buffer_factory, seed=seed,
+                                       batch_size=batch_size,
+                                       step_to_update=step_to_update, buffer_size=buffer_size,
+                                       gamma=gamma, episodes_window_size=episodes_window_size,
+                                       name=name)
 
         # Q-Network
         self.batch_size = batch_size  # initialize learning batch size
@@ -50,28 +66,19 @@ class DQNAgent:
         self.action_size = self.qnetwork_local.action_size()
 
         self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=lr)
-        self.gamma = gamma
         self.tau = tau
 
-        # Replay memory
-        self.memory = replay_buffer_factory.build(buffer_size, batch_size, seed)
+    @staticmethod
+    def soft_update(local_model: nn.Module, target_model: nn.Module, tau: float):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
 
-        # Initialize time step (for updating every self.steps_to_update steps)
-        self.t_step = 0
-        self.steps_to_update = step_to_update
-
-        # Agent history stats (collected during the env. exploration/solving)
-        self.scores = []  # list containing scores from each episode
-        self.scores_window = deque(maxlen=episodes_window_size)
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def buffer_size(self):
-        """ gets the current reply buffer size"""
-        return len(self.memory)
+        :param local_model: weights will be copied from
+        :param  target_model: weights will be copied to
+        :param tau: interpolation parameter
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
     def explore(self, env, extract_state, n_episodes=1800, max_t=1000, brain_name=None,
                 eps_start=1.0, eps_end=0.01, eps_decay=0.99):
@@ -79,8 +86,8 @@ class DQNAgent:
 
         :param env: the environment to solve
         :param extract_state: state extractor function (env-info->state [np.ndarray])
-        :param n_episodes: the max number of exploration episodes (the env expected to be solve before)
-        :param max_t: maximum number of steps/actions with in an episode (if termination is not reached)
+        :param n_episodes: the max number of exploration episodes (env. expected to be solve before)
+        :param max_t: maximum steps/actions within an episode (if termination is not reached)
         :param brain_name: unity env brain_name
         :param eps_start: initial epsilon for epsilon-greedy sampling
         :param eps_end: minimum epsilon for epsilon-greedy sampling
@@ -127,13 +134,14 @@ class DQNAgent:
                                                                    np.mean(self.scores_window)))
             if np.mean(self.scores_window) >= 13.0:
                 print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
-                    i_episode, np.mean(
+                    i_episode - 100, np.mean(
                         self.scores_window)))
                 self.save()
                 break
 
     def save(self):
-        """ save local qnetwork_local as state_dict"""
+        # TODO save the full agent to be reloaded (implement a save/load mechanism)
+        """ save local Q network_local as state_dict"""
         torch.save(self.qnetwork_local.state_dict(), '{}.chpnt.pth'.format(self._name))
 
     def target_local_abs_error(self, state, action: int,
@@ -150,8 +158,7 @@ class DQNAgent:
     def step(self, state, action, reward, next_state, done):
         """ Update the replay buffer and eventually train (each self.steps_to_update)"""
         self.memory.add(state, action, reward, next_state,
-                        done, self.target_local_abs_error(state,
-                                                          action))  # update replay buffer with the exp.
+                        done, self.target_local_abs_error(state, action))
         # Learn every self.steps_to_update steps.
         self.t_step = (self.t_step + 1) % self.steps_to_update
         if self.t_step == 0:
@@ -178,28 +185,12 @@ class DQNAgent:
 
         return random.choice(np.arange(self.action_size))
 
-    @staticmethod
-    def to_tensors(experiences: List[Experience]) -> Tuple[torch.Tensor]:
-        """ Transforms a batch of experiences to tensors for  training/learning"""
-        states = torch.from_numpy(
-            np.array([e.state for e in experiences])).float().to(DEVICE)
-        actions = torch.from_numpy(
-            np.vstack([e.action for e in experiences])).long().to(DEVICE)
-        rewards = torch.from_numpy(
-            np.vstack([e.reward for e in experiences])).float().to(DEVICE)
-        next_states = torch.from_numpy(
-            np.array([e.next_state for e in experiences])).float().to(DEVICE)
-        dones = torch.from_numpy(
-            np.vstack([e.done for e in experiences]).astype(np.uint8)).float().to(
-            DEVICE)
-        return states, actions, rewards, next_states, dones
-
     def learn(self, experiences: List[Experience]):
         """Update value parameters using given batch of experience tuples.
 
         :param experiences: tuple of (s, a, r, s', done) tuples
         """
-        states, actions, rewards, next_states, dones = DQNAgent.to_tensors(experiences)
+        states, actions, rewards, next_states, dones = to_tensors(experiences, DEVICE)
 
         # Get max predicted Q values (for next states) from target model
         q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
@@ -218,15 +209,3 @@ class DQNAgent:
 
         # soft-update target network
         DQNAgent.soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
-
-    @staticmethod
-    def soft_update(local_model: nn.Module, target_model: nn.Module, tau: float):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-
-        :param local_model: weights will be copied from
-        :param  target_model: weights will be copied to
-        :param tau: interpolation parameter
-        """
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
